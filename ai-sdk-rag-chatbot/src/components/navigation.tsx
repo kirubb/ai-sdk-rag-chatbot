79dcaa451d3cb3038dab84a0e19757d21894274a
Kirit Baruah <125810987+kirubb@users.noreply.github.com>
2025-12-07 23:16:33 +0530

    final changes

--- ai-sdk-rag-chatbot/src/components/navigation.tsx
+++ ai-sdk-rag-chatbot/src/components/navigation.tsx
@@ -6,17 +6,24 @@ import {
   SignedOut,
 } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Navigation = () => {
   return (
     <nav className="border-b border-(--foreground)/10">
       <div className="flex container h-16 items-center justify-between px-4  mx-auto">
-        <div className="text-xl font-semibold">RAG Chatbot</div>
+        
+        <Link 
+          href="/" 
+          className="inline-flex items-center text-2xl font-bold text-foreground transition-colors hover:text-muted-foreground/80"
+        >
+          RAG Chatbot
+        </Link>
 
         <div className="flex gap-2">
           <SignedOut>
             <SignInButton mode="modal">
-              <Button variant="ghost">Sign In</Button>
+              <Button variant="outline">Sign In</Button>
             </SignInButton>
             <SignUpButton mode="modal">
               <Button>Sign Up</Button>
